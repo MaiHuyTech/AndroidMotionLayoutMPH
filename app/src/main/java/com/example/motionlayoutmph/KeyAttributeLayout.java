161cff38f5ba812dc096a6ab6fbd20968d99704c
maihuytech@gmail.com <maihuytech@gmail.com>
2021-09-17 15:04:47 +0700

    Commit Mai Huy

---
+++ app/src/main/java/com/example/motionlayoutmph/KeyAttributeLayout.java
@@ -0,0 +1,14 @@
+package com.example.motionlayoutmph;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KeyAttributeLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_key_attribute_layout);
+    }
+}
